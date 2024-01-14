b8b6e15f176e16c978d96b840548fd2a932d1ea7
Pol Besalu <pol.besalu@eurofirms.com>
2024-01-14 19:40:10 +0100

    Agregar archivos de proyecto.

---
+++ FirebaseCloudManager/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Firebase.Auth.UI;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FirebaseCloudManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Force override culture & language
+            //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("cs");
+            //CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("cs");
+
+            // Firebase UI initialization
+            FirebaseUI.Initialize(new FirebaseUIConfig
+            {
+                ApiKey = "ygXv79yeunqsoI8YSirXyskeIx7EHyQq2_9gAYnHPmo",
+                AuthDomain = "provafirebase-e63fd.firebaseapp.com",
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new GoogleProvider(),
+                    new EmailProvider()
+                },
+                PrivacyPolicyUrl = "https://github.com/step-up-labs/firebase-authentication-dotnet",
+                TermsOfServiceUrl = "https://github.com/step-up-labs/firebase-database-dotnet",
+                IsAnonymousAllowed = true,
+                AutoUpgradeAnonymousUsers = true,
+                UserRepository = new FileUserRepository("FirebaseSample"),
+                // Func called when upgrade of anonymous user fails because the user already exists
+                // You should grab any data created under your anonymous user, sign in with the pending credential
+                // and copy the existing data to the new user
+                // see details here: https://github.com/firebase/firebaseui-web#upgrading-anonymous-users
+                AnonymousUpgradeConflict = conflict => conflict.SignInWithPendingCredentialAsync(true)
+            });
+        }
+    }
+}
---
+++ FirebaseCloudManager/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Firebase.Auth.UI.Pages;
+using Firebase.Auth.UI;
+using Firebase.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirebaseCloudManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FirebaseUI.Instance.Client.AuthStateChanged += this.AuthStateChanged;
+        }
+
+        private void AuthStateChanged(object sender, UserEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (e.User == null)
+                {
+                    //await FirebaseUI.Instance.Client.SignInAnonymouslyAsync();
+                    this.Frame.Navigate(new LoginPage());
+                }
+                else if (e.User.IsAnonymous)
+                {
+                    this.Frame.Navigate(new LoginPage());
+                }
+               /* else if ((this.Frame.Content == null || this.Frame.Content.GetType() != typeof(MainPage)))
+                {
+                    this.Frame.Navigate(new MainPage());
+                }*/
+            });
+        }
+    }
+}
