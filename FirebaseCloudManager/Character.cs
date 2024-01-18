bfd0fe6274bb3647ffe56347e51ff7ad8470fd96
Pol Besalu <pol.besalu@eurofirms.com>
2024-01-18 20:32:01 +0100

    fak

---
+++ FirebaseCloudManager/Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebaseCloudManager
+{
+    class Character
+    {
+        private string nom;
+        private string serie;
+        private string descripcio;
+        private string data;
+        private string fotoURL;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Serie { get => serie; set => serie = value; }
+        public string Descripcio { get => descripcio; set => descripcio = value; }
+        public string Data { get => data; set => data = value; }
+        public string FotoURL { get => fotoURL; set => fotoURL = value; }
+
+
+        public Character(string nom, string serie, string descripcio, string data, string fotoURL)
+        {
+            this.nom = nom;
+            this.serie = serie;
+            this.descripcio = descripcio;
+            this.data = data;
+            this.fotoURL = fotoURL;
+        }
+    }
+}
---
+++ FirebaseCloudManager/CharacterService.cs
@@ -0,0 +1,45 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebaseCloudManager
+{
+    class CharacterService
+    {
+        private const string FirebaseDatabaseUrl = "https://provadam-15e08-default-rtdb.europe-west1.firebasedatabase.app/";
+        private readonly FirebaseClient firebaseClient;
+        public CharacterService()
+        {
+            firebaseClient = ClientCreator.GetFirebaseClient(FirebaseDatabaseUrl, "2Ex9nEJ9vIcm4j8gB9SNN5e0Hb33");
+        }
+        public async Task AddCharacter(Character character)
+        {
+            await firebaseClient
+            .Child("Characters")
+            .PostAsync(character);
+        }
+        public async Task<List<Character>> GetCharacters()
+        {
+            var characters = await firebaseClient.Child("Characters").OnceAsync<Character>();
+            foreach (var character in characters)
+            {
+                character.Object.Nom = character.Key;
+            }
+            return characters.Select(firebaseObject => firebaseObject.Object).ToList();
+        }
+        public List<Character> GetAllCharacters()
+        {
+            return GetCharacters().Result;
+        }
+        public static FirebaseClient GetFirebaseClient(string url, string secret = null)
+        {
+            return new FirebaseClient(url, new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(secret)
+            });
+        }
+    }
+}
